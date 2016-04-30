6bdba470f9146bbd15b1f9d88f24a1a1c048eb7b
hshs <freegate22@hotmail.com>
2016-04-30 21:14:06 +0900

    동적페이지

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  var date = new Date();
+  for(var i = 0; i < 5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  var output = `<!DOCTYPE html>
+  <html>
+  <body>
+    Hello, Dynamic!!
+    <ul>
+    ${lis}
+    </ul>
+    ${date}
+  </body>
+  </html>
+`
+  res.send(output);
+})
 app.get('/route', function(req, res){
   res.send('hello apple, <img src="/apple.jpg">');
 })
