d84876f87d0c9bc2044d4a63278d046dc7eea568
hshs <freegate22@hotmail.com>
2016-04-30 18:05:16 +0900

    정적 페이지 표시하기

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/route', function(req, res){
+  res.send('hello apple, <img src="/apple.jpg">');
+})
 app.get('/login', function (req, res) {
   res.send('Login Please');
 });
