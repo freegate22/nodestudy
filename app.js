1246eee0fd4964e9bd25ccfcdb1e1a4cf30954bf
hshs <freegate22@hotmail.com>
2016-04-30 22:52:43 +0900

    jade

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+app.locals.pretty = true;
+app.set('views','./views'); // 관습적 (default 값이기도 함)
+app.set('view engine','jade');  // 변경불가
+app.use(express.static('public')); // 관습적
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+app.get('/template', function(req, res){
+  res.render('temp', {time:Date(), _title:'Jade'});
 });
 app.get('/dynamic', function(req, res){
   var lis = '';
